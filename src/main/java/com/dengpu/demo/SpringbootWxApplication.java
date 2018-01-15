27a69a69f9e1e4fd101eaa7c8194a730258ee19e
dengpu <dengpu90@sina.com>
2018-01-16 00:11:02 +0800

    代码

---
+++ src/main/java/com/dengpu/demo/SpringbootWxApplication.java
@@ -0,0 +1,12 @@
+package com.dengpu.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWxApplication.class, args);
+	}
+}
